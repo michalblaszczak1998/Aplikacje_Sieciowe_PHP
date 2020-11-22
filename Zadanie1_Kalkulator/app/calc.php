553a29d61db37101b67c110749fc668bf57aab93
Michu <Michu@DESKTOP-GBEKG3V>
2020-11-22 13:45:20 +0100

    Koniec

--- Zadanie1_Kalkulator/app/calc.php
+++ Zadanie1_Kalkulator/app/calc.php
@@ -1,73 +1,26 @@
 <?php
-// KONTROLER strony kalkulatora
-require_once dirname(__FILE__).'/../config.php';
+    require_once dirname(__FILE__).'/../config.php';
 
-// W kontrolerze niczego nie wysyła się do klienta.
-// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
-// Parametry do widoku przekazujemy przez zmienne.
+    $loanValue = $_POST['loanValue'];
+    $yearsOfCredit = $_POST['yearsOfCredit'];
+    $percent = $_POST['percent'];
 
-// 1. pobranie parametrów
+    if (!(isset($loanValue) && isset($yearsOfCredit) && isset($percent))) {
+        $errorMessages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+    }
 
-$x = $_REQUEST ['x'];
-$y = $_REQUEST ['y'];
-$operation = $_REQUEST ['op'];
+    if (empty($loanValue)) {
+        $errorMessages [] = 'Nie podano kwoty kredytu.';
+    }
+    if (empty($yearsOfCredit)) {
+        $errorMessages [] = 'Nie podano na ile lat jest kredyt.';
+    }
+    if (empty($percent)) {
+        $errorMessages [] = 'Nie podano jakie jest oprocentowanie.';
+    }
 
-// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+    if (empty($errorMessages)) {
+        $result = ($loanValue / $yearsOfCredit * 12 * ($percent / 100)) + $loanValue / $yearsOfCredit * 12;
+    }
 
-// sprawdzenie, czy parametry zostały przekazane
-if ( ! (isset($x) && isset($y) && isset($operation))) {
-	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
-	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
-}
-
-// sprawdzenie, czy potrzebne wartości zostały przekazane
-if ( $x == "") {
-	$messages [] = 'Nie podano liczby 1';
-}
-if ( $y == "") {
-	$messages [] = 'Nie podano liczby 2';
-}
-
-//nie ma sensu walidować dalej gdy brak parametrów
-if (empty( $messages )) {
-	
-	// sprawdzenie, czy $x i $y są liczbami całkowitymi
-	if (! is_numeric( $x )) {
-		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
-	}
-	
-	if (! is_numeric( $y )) {
-		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
-	}	
-
-}
-
-// 3. wykonaj zadanie jeśli wszystko w porządku
-
-if (empty ( $messages )) { // gdy brak błędów
-	
-	//konwersja parametrów na int
-	$x = intval($x);
-	$y = intval($y);
-	
-	//wykonanie operacji
-	switch ($operation) {
-		case 'minus' :
-			$result = $x - $y;
-			break;
-		case 'times' :
-			$result = $x * $y;
-			break;
-		case 'div' :
-			$result = $x / $y;
-			break;
-		default :
-			$result = $x + $y;
-			break;
-	}
-}
-
-// 4. Wywołanie widoku z przekazaniem zmiennych
-// - zainicjowane zmienne ($messages,$x,$y,$operation,$result)
-//   będą dostępne w dołączonym skrypcie
-include 'calc_view.php';
+    include 'calc_view.php';

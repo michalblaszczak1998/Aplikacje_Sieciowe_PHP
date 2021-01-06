b07f2d030d775e6ac0bfa419a58cfd674442a87e
michalblaszczak1998 <74820923+michalblaszczak1998@users.noreply.github.com>
2021-01-06 14:38:20 +0100

    Add files via upload
    Niestety konieczne będzie dodanie smarty do folderu lib przez wzgląd na zbyt dużą ilość plików :(
    Git stats

---
+++ Zadanie7_Ochrona/app/views/CalcView.tpl
@@ -0,0 +1,27 @@
+{extends file="main.tpl"}
+
+{block name=content}
+    <div class="pure-menu pure-menu-horizontal bottom-margin">
+        <a href="{$conf->action_url}logout" class="pure-menu-heading pure-menu-link">wyloguj</a>
+        <span style="float:right;">użytkownik: {$user->login}, rola: {$user->role}</span>
+    </div>
+    <form class="pure-form pure-form-stacked" action="{$conf->action_root}calcCompute" method="post">
+        <fieldset>
+            <label>Kwota kredytu: </label>
+            <input type="number" placeholder="Kwota pożyczki" min="1" name="loanValue" value="{$form->loanValue}"/><br/>
+            <label>Na ile lat: </label>
+            <input type="number" min="1" placeholder="Okres" name="yearsOfCredit" value="{$form->yearsOfCredit}"/><br/>
+            <label>Oprocentowanie: </label>
+            <input type="number" min="1" placeholder="Oprocentowanie" name="percent" value="{$form->percent}"/><br/>
+        </fieldset>
+        <button type="submit" class="pure-button pure-button-primary">Oblicz</button>
+    </form>
+    {include file='messages.tpl'}
+
+    {if isset($res->result)}
+        <div class="messages info">
+            Miesięczna rata będzie wynosić: {$res->result}zł
+        </div>
+    {/if}
+
+{/block}
---
+++ Zadanie7_Ochrona/templates_c/15ba7fc8245b5ddd7f93c4ddf0956c41c2b5d2df_0.file.CalcView.tpl.php
@@ -0,0 +1,79 @@
+<?php
+/* Smarty version 3.1.30, created on 2020-12-25 17:26:28
+  from "C:\xampp\htdocs\php_07_role\app\views\CalcView.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5fe612b4a78af7_27960069',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '15ba7fc8245b5ddd7f93c4ddf0956c41c2b5d2df' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\php_07_role\\app\\views\\CalcView.tpl',
+      1 => 1608913560,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:main.tpl' => 1,
+    'file:messages.tpl' => 1,
+  ),
+),false)) {
+function content_5fe612b4a78af7_27960069 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_20845781475fe612b4a78532_05877399', 'content');
+$_smarty_tpl->inheritance->endChild();
+$_smarty_tpl->_subTemplateRender("file:main.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, false);
+}
+/* {block 'content'} */
+class Block_20845781475fe612b4a78532_05877399 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+    <div class="pure-menu pure-menu-horizontal bottom-margin">
+        <a href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
+logout" class="pure-menu-heading pure-menu-link">wyloguj</a>
+        <span style="float:right;">użytkownik: <?php echo $_smarty_tpl->tpl_vars['user']->value->login;?>
+, rola: <?php echo $_smarty_tpl->tpl_vars['user']->value->role;?>
+</span>
+    </div>
+    <form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+calcCompute" method="post">
+        <fieldset>
+            <label>Kwota kredytu: </label>
+            <input type="number" placeholder="Kwota pożyczki" min="1" name="loanValue" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->loanValue;?>
+"/><br/>
+            <label>Na ile lat: </label>
+            <input type="number" min="1" placeholder="Okres" name="yearsOfCredit" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->yearsOfCredit;?>
+"/><br/>
+            <label>Oprocentowanie: </label>
+            <input type="number" min="1" placeholder="Oprocentowanie" name="percent" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->percent;?>
+"/><br/>
+        </fieldset>
+        <button type="submit" class="pure-button pure-button-primary">Oblicz</button>
+    </form>
+    <?php $_smarty_tpl->_subTemplateRender("file:messages.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
+
+    <?php if (isset($_smarty_tpl->tpl_vars['res']->value->result)) {?>
+        <div class="messages info">
+            Miesięczna rata będzie wynosić: <?php echo $_smarty_tpl->tpl_vars['res']->value->result;?>
+zł
+        </div>
+    <?php }?>
+
+<?php
+}
+}
+/* {/block 'content'} */
+}
